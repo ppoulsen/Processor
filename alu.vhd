e58f0495ab33d81b7011152b9af883d78acfd799
Paul Poulsen <ppoulsen@css-design.com>
2012-12-11 01:20:37 -0600

    ALU Zero/LT Flags Added
    Untested

--- alu.vhd
+++ alu.vhd
@@ -6,11 +6,13 @@ ENTITY alu IS
 	PORT(x, y : IN STD_LOGIC_VECTOR(7 DOWNTO 0); -- input args
 		opcode : IN STD_LOGIC_VECTOR(2 DOWNTO 0); -- opcodes for defined in excel
 	    result : OUT STD_LOGIC_VECTOR(7 DOWNTO 0)); -- result
+	    less_than : OUT STD_LOGIC_VECTOR(7 DOWNTO 0));
+	    zero : OUT BIT;
 END alu;
 
 ARCHITECTURE dataflow OF alu IS
 BEGIN
-	PROCESS(opcode) -- alter values on opcode change
+	PROCESS(x, y, opcode) -- alter values on change
 	VARIABLE to_shift: UNSIGNED := UNSIGNED(x);
 	VARIABLE shift: INTEGER := TO_INTEGER(UNSIGNED(y));
 	VARIABLE shifted: UNSIGNED;
@@ -30,8 +32,19 @@ BEGIN
 				result <= STD_LOGIC_VECTOR(UNSIGNED(x) + UNSIGNED(y));
 			WHEN "110" =>
 				result <= STD_LOGIC_VECTOR(UNSIGNED(x) - UNSIGNED(y));
+				CASE result(7) IS
+					WHEN '0' =>
+						less_than <= "00000000";
+					WHEN OTHERS =>
+						less_than <= "00000001";
+				END CASE;
 			WHEN OTHERS =>
 				result <= "00000000";
 		END CASE;
+		IF result = "00000000" THEN
+			zero = '1';
+		ELSE
+			zero = '0';
+		END IF;
 	END PROCESS;
 END dataflow;
