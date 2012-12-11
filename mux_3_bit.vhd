3b87eb56bf94e6681fcd9390af1a1377683738a4
Brian Clymer <crdnilfan@gmail.com>
2012-12-11 16:16:04 -0600

    3-bit mux

---
+++ mux_3_bit.vhd
@@ -0,0 +1,23 @@
+LIBRARY ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+ENTITY mux_3bit IS
+	PORT(	mux2_a : IN STD_LOGIC_VECTOR(2 DOWNTO 0);
+			mux2_b : IN STD_LOGIC_VECTOR(2 DOWNTO 0);
+			mux2_choice : IN BIT;
+			mux2_out : OUT STD_LOGIC_VECTOR(2 DOWNTO 0)
+		);
+END mux_3bit;
+
+ARCHITECTURE dataflow OF mux_3bit IS
+BEGIN
+	PROCESS (mux2_choice)
+	BEGIN
+		if (mux2_choice = '0') THEN
+			mux2_out <= mux2_a;
+		ELSE
+			mux2_out <= mux2_b;
+		END IF;
+	END PROCESS;
+END dataflow;
