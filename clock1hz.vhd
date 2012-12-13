3e8ec1f678e1836f8d1f0221b01ce1edf6b7978f
Brian Clymer <crdnilfan@gmail.com>
2012-12-12 20:47:37 -0600

    LED!!!!!!!!!!1!!!!!!!!!!!111!!!!1!1!!!!!!s

--- clock1hz.vhd
+++ clock1hz.vhd
@@ -5,44 +5,70 @@ ENTITY Clock1Hz IS
 		clock0:	OUT BIT;
 		clock1:	OUT BIT;
 		clock2:	OUT BIT;
-		clock3:	OUT BIT
+		clock3:	OUT BIT;
+		clock4:	OUT BIT;
+		clock5: OUT BIT
         );
 END Clock1Hz;
 
 ARCHITECTURE a OF Clock1Hz IS
 BEGIN
 PROCESS (reset, clk)
-	VARIABLE big_count : INTEGER RANGE 0 TO 3146;
-    VARIABLE cnt : INTEGER RANGE 0 TO 3;
+	VARIABLE big_count : INTEGER RANGE 0 TO 2000;
+    VARIABLE cnt : INTEGER RANGE 0 TO 5;
 BEGIN
     IF (reset = '1') THEN
         cnt := 0;
 	ELSIF (clk'EVENT AND clk = '1') THEN
-		IF(big_count = 3146) THEN
+		IF(big_count = 2000) THEN
 			big_count := 0;
 			IF (cnt = 0) THEN
 				clock0 <= '1';
 				clock2 <= '0';
 				clock1 <= '0';
 				clock3 <= '0';
+				clock4 <= '0';
+				clock5 <= '0';
 				cnt := 1;
 			ELSIF (cnt = 1) THEN
 				clock1 <= '1';
 				clock2 <= '0';
 				clock0 <= '0';
 				clock3 <= '0';
+				clock4 <= '0';
+				clock5 <= '0';
 				cnt := 2;
 			ELSIF (cnt = 2) THEN
 				clock2 <= '1';
 				clock0 <= '0';
 				clock1 <= '0';
 				clock3 <= '0';
+				clock4 <= '0';
+				clock5 <= '0';
 				cnt := 3;
 			ELSIF (cnt = 3) THEN
 				clock3 <= '1';
 				clock0 <= '0';
 				clock1 <= '0';
 				clock2 <= '0';
+				clock4 <= '0';
+				clock5 <= '0';
+				cnt := 4;
+			ELSIF (cnt = 4) THEN
+				clock3 <= '0';
+				clock0 <= '0';
+				clock1 <= '0';
+				clock2 <= '0';
+				clock4 <= '1';
+				clock5 <= '0';
+				cnt := 5;
+			ELSIF (cnt = 5) THEN
+				clock3 <= '0';
+				clock0 <= '0';
+				clock1 <= '0';
+				clock2 <= '0';
+				clock4 <= '0';
+				clock5 <= '1';
 				cnt := 0;
 			END IF;
 		ELSE
--- mem_intercept.vhd
+++ mem_intercept.vhd
@@ -7,6 +7,7 @@ ENTITY mem_intercept IS
 		data_in : IN STD_LOGIC_VECTOR(7 DOWNTO 0);
 		sw : IN BIT;
 		lw : IN BIT;
+		clk_in : IN STD_LOGIC;
 	    display_1_out : OUT STD_LOGIC_VECTOR(3 DOWNTO 0);
 	    display_2_out : OUT STD_LOGIC_VECTOR(3 DOWNTO 0);
 	    ram_a_out : OUT STD_LOGIC_VECTOR(7 DOWNTO 0);
@@ -19,24 +20,26 @@ END mem_intercept;
 
 ARCHITECTURE dataflow OF mem_intercept IS
 BEGIN
-	PROCESS (address_in, data_in, sw, lw)
+	PROCESS (clk_in)
 	BEGIN
-		IF (sw = '0' AND lw = '1') THEN
-			CASE TO_INTEGER(UNSIGNED(address_in)) IS
-				when 0 => read_switch <= "00";
-				when 1 => read_switch <= "01";
-				when 2 => read_switch <= "10";
-				when others => read_switch <= "11";
-			END CASE;
-		ELSIF (sw = '1' AND lw = '0') THEN
-			CASE TO_INTEGER(UNSIGNED(address_in)) IS
-				when 0 => display_1_out <= data_in(7 DOWNTO 4); display_2_out <= data_in(3 DOWNTO 0);
-				when 1 => decimal_1 <= data_in(0);
-				when 2 => decimal_2 <= data_in(0);
-				when others => null;
-			END CASE;
+		IF (RISING_EDGE(clk_in)) THEN
+			IF (sw = '0' AND lw = '1') THEN
+				CASE TO_INTEGER(UNSIGNED(address_in)) IS
+					when 0 => read_switch <= "00";
+					when 1 => read_switch <= "01";
+					when 2 => read_switch <= "10";
+					when others => read_switch <= "11";
+				END CASE;
+			ELSIF (sw = '1' AND lw = '0') THEN
+				CASE TO_INTEGER(UNSIGNED(address_in)) IS
+					when 0 => display_1_out <= data_in(7 DOWNTO 4); display_2_out <= data_in(3 DOWNTO 0);
+					when 1 => decimal_1 <= data_in(0);
+					when 2 => decimal_2 <= data_in(0);
+					when others => null;
+				END CASE;
+			END IF;
+			ram_a_out <= address_in;
+			ram_d_out <= data_in;
 		END IF;
-		ram_a_out <= address_in;
-		ram_d_out <= data_in;
 	END PROCESS;
 END dataflow;
