4cd5d89a8cf01f0c5d1324d0cf3ad65eb81079b2
Brian Clymer <crdnilfan@gmail.com>
2012-12-13 19:56:45 -0600

    Clock fix

--- clock1hz.vhd
+++ clock1hz.vhd
@@ -31,6 +31,8 @@ BEGIN
 				clock3 <= '0';
 				clock4 <= '0';
 				clock5 <= '0';
+				clock6 <= '0';
+				clock7 <= '0';
 				cnt := 1;
 			ELSIF (cnt = 1) THEN
 				clock1 <= '1';
@@ -39,6 +41,8 @@ BEGIN
 				clock3 <= '0';
 				clock4 <= '0';
 				clock5 <= '0';
+				clock6 <= '0';
+				clock7 <= '0';
 				cnt := 2;
 			ELSIF (cnt = 2) THEN
 				clock2 <= '1';
@@ -47,6 +51,8 @@ BEGIN
 				clock3 <= '0';
 				clock4 <= '0';
 				clock5 <= '0';
+				clock6 <= '0';
+				clock7 <= '0';
 				cnt := 3;
 			ELSIF (cnt = 3) THEN
 				clock3 <= '1';
@@ -55,6 +61,8 @@ BEGIN
 				clock2 <= '0';
 				clock4 <= '0';
 				clock5 <= '0';
+				clock6 <= '0';
+				clock7 <= '0';
 				cnt := 4;
 			ELSIF (cnt = 4) THEN
 				clock3 <= '0';
@@ -63,6 +71,8 @@ BEGIN
 				clock2 <= '0';
 				clock4 <= '1';
 				clock5 <= '0';
+				clock6 <= '0';
+				clock7 <= '0';
 				cnt := 5;
 			ELSIF (cnt = 5) THEN
 				clock3 <= '0';
@@ -71,6 +81,8 @@ BEGIN
 				clock2 <= '0';
 				clock4 <= '0';
 				clock5 <= '1';
+				clock6 <= '0';
+				clock7 <= '0';
 				cnt := 6;
 			ELSIF (cnt = 6) THEN
 				clock3 <= '0';
